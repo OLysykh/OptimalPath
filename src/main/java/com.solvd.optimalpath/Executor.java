d90cfceb53527fff6f32dd9bc2e9bcd0c729fd61
Lysykh Oleksandr <a.lyssykh@gmail.com>
2022-05-30 23:32:25 +0300

    initial push!

---
+++ src/main/java/com.solvd.optimalpath/Executor.java
@@ -0,0 +1,13 @@
+package com.solvd.optimalpath;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Executor {
+
+    private static final Logger LOGGER = LogManager.getLogger(Executor.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Let`s do it together!");
+    }
+}
