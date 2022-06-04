23176de9df47b44967b0d28b901141a9abc7900b
WINEWOOD531 <taras.odynyuk@gmail.com>
2022-06-04 10:28:00 +0300

    add connection and dao

---
+++ src/main/java/com.solvd.optimalpath/models/CitiesModel.java
@@ -0,0 +1,60 @@
+package com.solvd.optimalpath.models;
+
+public class CitiesModel {
+    private int id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    public CitiesModel() {
+    }
+
+    public CitiesModel(int id, String name, Double latitude, Double longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "CitiesModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
